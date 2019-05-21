f1dd827857cb6403f162f6567f00f1aaf7abfaab
JBrognoli <brognolijoao@hotmail.com>
2019-05-21 17:31:48 -0300

    feat(vuex): implement vuex structure

---
+++ src/components/BaseColumn.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "BaseColumn"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BaseDeck.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "BaseDeck"
+  }
+</script>
+
+<style scoped>
+
+</style>
